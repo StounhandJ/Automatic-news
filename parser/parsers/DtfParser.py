afa122061fa6eb6a36e653a01e03cc61235927e4
StounhandJ <rmarinicev@gmail.com>
2021-07-29 13:23:23 +0300

    fix parser DTF

--- parser/parsers/DtfParser.py
+++ parser/parsers/DtfParser.py
@@ -8,8 +8,8 @@ from data.Abstract.ParserAbstract import ParserAbstract
 
 
 class DTFParser(ParserAbstract):
-    _urlOrigin = "https://dtf.ru/games"
-    _urlMore = "https://dtf.ru/games/more"
+    _urlOrigin = "https://dtf.ru/games/entries/new?mode=ajax"
+    _urlMore = "https://dtf.ru/games/entries/new/more"
     _lastTitle = ""
     _className = ""
     _lastID = 0
@@ -21,51 +21,67 @@ class DTFParser(ParserAbstract):
     async def parse(self) -> [Article]:
         articles = []
 
-        firstID = self._getFirstID()
-        feedValue = self._getFeedLastSortingValue()
+        html = self._createDictFromJson(self._urlOrigin)["module.ajaxify"]["html"]
+        pageBS = self._createSoup(html)
 
-        page = 1
-        articles += self._parseData(firstID, feedValue)
+        feedValue = self._getFeedLastSortingValue(pageBS)
+        lastID = self._getLastID(pageBS)
 
-        while self._isGetLastArticle(articles, page) and self._lastTitle != "":
-            articles += self._parseData(self._lastID, feedValue-page)
+        page = 0
+        while self._isGetLastArticle(articles, page):
+            if page != 0:
+                pageBS = self._getPage(lastID, feedValue)
+                lastID, feedValue = self._getLastIdAndLastSortingValue(lastID, feedValue)
+            articles += self._parseData(pageBS)
             page += 1
+            if self._lastTitle == "":
+                break
 
         self._setLastArticle(articles[0]) if len(articles) > 0 else 0
         return articles
 
-    def _getFirstID(self) -> int:
+    def _getLastID(self, pageBS: BeautifulSoup) -> int:
         """
-        Получить id самого первого поста
+        Получить id последнего поста
         :return:
         """
-        soup = self._createSoupFromUrl(self._urlOrigin)
-        firstArticle = soup.findAll(class_="feed__item l-island-round")[0]
-        return firstArticle.findAll("div")[0]["data-content-id"]
+        return int(pageBS.findAll("div", class_="feed")[0]["data-feed-last-id"])
 
-    def _getFeedLastSortingValue(self) -> float:
+    def _getFeedLastSortingValue(self, pageBS: BeautifulSoup) -> float:
         """
         Получить значения поиска, специальный обязательные параметр
         :return:
         """
-        soup = self._createSoupFromUrl(self._urlOrigin)
-        return float(soup.findAll("div", class_="feed")[0]["data-feed-last-sorting-value"].replace(",","."))
+        return float(pageBS.findAll("div", class_="feed")[0]["data-feed-last-sorting-value"].replace(",", "."))
 
-    def _parseData(self, lastID: int, feedValue: float) -> [Article]:
+    def _getPage(self, lastID: int, feedValue: float) -> BeautifulSoup:
         """
-        Получить все посты после поста с lastID. Обращаясь к скрытой api сайта
-        :param lastID: ID поста после которого искать
-        :param feedValue: Специальный обязательные параметр
+            Получить страницу после поста с lastID. Обращаясь к скрытой api сайта
+            :param lastID: ID поста после которого искать
+            :param feedValue: Специальный обязательные параметр
+            :return:
+        """
+        return self._createSoup(self._createDictFromJson(self._urlMore, {
+            "last_id": lastID, "last_sorting_value": feedValue
+        })["data"]["items_html"])
+
+    def _getLastIdAndLastSortingValue(self, lastID: int, feedValue: float):
+        data = self._createDictFromJson(self._urlMore, {
+            "last_id": lastID, "last_sorting_value": feedValue
+        })["data"]
+        return data["last_id"], data["last_sorting_value"]
+
+
+    def _parseData(self, pageBS: BeautifulSoup) -> [Article]:
+        """
+        Получить все посты поста с страницы.
+        :param pageBS: Страница с статьями
         :return:
         """
         articles = []
 
-        pageHTML = self._createSoup(self._createDictFromJson(self._urlMore, {
-            "last_id": lastID, "last_sorting_value": feedValue
-        })["data"]["items_html"])
-
-        for articleHTML in pageHTML.findAll(class_="feed__item l-island-round"):
-            article, self._lastID = self._articleHtmlToArticle(
+        for articleHTML in pageBS.findAll(class_="feed__item l-island-round"):
+            article = self._articleHtmlToArticle(
                 articleHTML)  # Получение поста и установка id последнего проверенного поста
             if self._isLastArticle(article):
                 break
@@ -73,7 +89,7 @@ class DTFParser(ParserAbstract):
 
         return articles
 
-    def _articleHtmlToArticle(self, articleHTML: Tag) -> [Article, int]:
+    def _articleHtmlToArticle(self, articleHTML: Tag) -> Article:
         """
         Парсинг отдельного поста.
         :param articleHTML:
@@ -82,19 +98,23 @@ class DTFParser(ParserAbstract):
         title = articleHTML.findAll(class_="content-title")[0].text.strip()
         src = articleHTML.findAll(class_="content-feed__link")[0]["href"]
         text = ""  # self._parseContentArticle(src)
+
         imgDivs = articleHTML.findAll(class_="andropov_image")
+        videoDivs = articleHTML.findAll(class_="andropov_video")
+        img_src = ""
         if len(imgDivs) > 0:
             img_src = imgDivs[0]["data-image-src"]
-        else:
-            img_src = articleHTML.findAll(class_="andropov_video")[0]["data-video-thumbnail"]
+        elif len(videoDivs) > 0:
+            img_src = videoDivs[0]["data-video-thumbnail"]
+
         id = articleHTML.findAll(class_="content-feed")[0]["data-content-id"]
-        return [ArticleFactory.create({
+        return ArticleFactory.create({
             "title": title,
             "src": src,
             "text": text,
             "img_src": img_src,
             "parser": self._className
-        }), id]
+        })
 
     def _isLastArticle(self, article) -> bool:
         """
